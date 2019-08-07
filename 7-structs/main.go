c71f7a822027c38a79dbe4a622c575447a63757f
Thao_Koios <thao.ho@koiostechnology.co.uk>
2019-08-07 15:50:54 +0100

    Structs exercise

--- 7-structs/main.go
+++ 7-structs/main.go
@@ -2,20 +2,35 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
-	firstName string
-	lastName  string
+	firstName   string
+	lastName    string
+	contactInfo // equivalent to defining like: contactInfo contactInfo
 }
 
 func main() {
-	// alex := person{firstName: "Alex", lastName: "Le"}
-	// fmt.Println(alex)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	//jimPointer := &jim // jim is the reference to the struct in memory, the actual value of the struct
+	jim.updateName("jimmy")
+	jim.print()
+}
 
-	var alex person
+func (pointerToPerson *person) updateName(newFirstName string) { // *person is a type description - it means updateName() function can only be called with the 																		 // receiver of a pointer to person (in this case, it is jimPointer)
+	(*pointerToPerson).firstName = newFirstName
+}
 
-	alex.firstName = "Alex"
-	alex.lastName = "Le"
-
-	fmt.Println(alex)
-	fmt.Printf("%+v\n", alex)
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
 }
