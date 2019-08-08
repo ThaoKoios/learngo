6384a0501643265564df371bb0fbeecdf21d7e54
Thao_Koios <thao.ho@koiostechnology.co.uk>
2019-08-08 15:39:36 +0100

    Map exercises

--- 8-maps/main.go
+++ 8-maps/main.go
@@ -3,10 +3,24 @@ package main
 import "fmt"
 
 func main() {
+	// var colors map[string]string
+
+	// colors := make(map[int]string) // create an empty map
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4bf745",
+		"white": "#ffffff",
 	}
+	// colors[10] = "#ffffff"
 
-	fmt.Println(colors)
+	// delete(colors, 10)
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
